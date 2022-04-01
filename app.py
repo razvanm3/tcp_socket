26dcfb5c4853d7cd7b19daa7aa83b57ddc2d4c63
Razvan Mihai <42058259+razvanm3@users.noreply.github.com>
2022-04-01 13:09:20 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import io
+import socket
+
+HOST = "192.168.0.104"  # Standard loopback interface address (localhost)
+PORT = 5000  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if data:
+                print(data)
